d1054f8948d57551f7114a50ce9a6b1f68e2489c
Yao Trezise <yao.trezise@gmail.com>
2025-05-05 19:16:54 +0200

    données

--- Main.py
+++ Main.py
@@ -17,7 +17,9 @@ if not os.path.exists("data"):    ### Création d'un dossier data et de sous dos
     os.makedirs("data/data")
 if not os.path.exists("data/history/sp500.txt"):
     with open("data/history/sp500.txt","w") as sp:
-        initialisation = str(int(pd.to_datetime("2000-01-01")))
+        initialisation = "2000-01-01 00:00:00"
+        initialisation = np.datetime64(initialisation,'s')
+        initialisation = str(initialisation.astype("int64"))
         sp.write(initialisation)
 with open("data/history/sp500.txt","r") as sp:
     row = sp.readlines()
@@ -36,8 +38,15 @@ if last_line//86400 != start_time//86400:                 ### On actualise les d
     sp_indice_data = yf.download("ES=F", period="3mo",interval="1d")[["Open","Close"]]
     sp_data.to_csv("data/data/sp_data.csv")                   ### Enregistrement des données en csv pour ne pas avoir à  ### 
     sp_indice_data.to_csv("data/data/sp_indice_data.csv")     ### les retélécherger plusieurs fois par jour              ###
+    with open("data/history/sp500.txt","a") as sp:
+        note = str(np.int64(time.time()))
+        sp.write(f"\n{note}")
     print("Données actualisées depuis Yfinance")
-else:
-    sp_data = pd.read_csv("data/data/sp_data.csv")                ### Dans le cas où la dernière fois qu'on à lancé le   ###
-    sp_indice_data = pd.read_csv("data/data/sp_indice_data.csv")  ### programme est dans la même journée qu'aujourd'hui, ###
-                                                                  ### on importe depuis le dossier data/data             ###
+else:                                                             ### Dans le cas où la dernière fois qu'on à lancé le   ###
+    sp_data = pd.read_csv(                                        ### programme est dans la même journée qu'aujourd'hui, ###
+        "data/data/sp_data.csv",                                  ### on importe les données depuis le dossier data/data ###
+        index_col=[0],header=[0,1]
+        )                
+    sp_indice_data = pd.read_csv("data/data/sp_indice_data.csv",index_col=[0],header=[0,1])                                  
+    sp_data.index = pd.to_datetime(sp_data.index)                 
+    sp_indice_data.index = pd.to_datetime(sp_indice_data.index)
